c8e05f39837819b8d318f260dec865e0d58ceab6
foundsunidhi <sunidhimundra05@gmail.com>
2025-05-10 21:16:23 +0530

    Refactor auth routes

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -3,46 +3,41 @@ import { register, login, updateEmpCode, changePassword, getUserProfile } from "
 
 const router = express.Router();
 
-// POST route for registration
 router.post("/register", async (req: Request, res: Response) => {
   try {
-    await register(req, res);  // Calling the register function
+    await register(req, res);  
   } catch (error) {
     res.status(500).json({ message: "Server Error", error });
   }
 });
 
-// POST route for login
 router.post("/login", async (req: Request, res: Response) => {
   try {
-    await login(req, res);  // Calling the login function
+    await login(req, res);  
   } catch (error) {
     res.status(500).json({ message: "Server Error", error });
   }
 });
-
 
 router.post("/change-password/:empCode", async (req: Request, res: Response) => {
   try {
-    await changePassword(req, res);  // Calling the changePassword function
+    await changePassword(req, res);  
   } catch (error) {
     res.status(500).json({ message: "Server Error", error });
   }
 }); 
 
-
 router.put("/update-empcode/:id", async (req: Request, res: Response) => {
   try {
-    await updateEmpCode(req, res);  // Calling the updateEmpCode function
+    await updateEmpCode(req, res);  
   } catch (error) {
     res.status(500).json({ message: "Server Error", error });
   }
 });
 
-
 router.get("/profile/:empCode", async (req: Request, res: Response) => {
   try {
-    await getUserProfile(req, res);  // Calling the getUserProfile function
+    await getUserProfile(req, res);  
   } catch (error) {
     res.status(500).json({ message: "Server Error", error });
   }
